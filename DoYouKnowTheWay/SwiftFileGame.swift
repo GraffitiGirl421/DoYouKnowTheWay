dc43c76017748c74444a8783471e215f90e22858
KimJongSkill420 <mr.pigslyz@gmail.com>
2018-01-23 09:56:30 -0500

    sample text

--- DoYouKnowTheWay/SwiftFileGame.swift
+++ DoYouKnowTheWay/SwiftFileGame.swift
@@ -1,9 +1,22 @@
-//
-//  SwiftFileGame.swift
-//  DoYouKnowTheWay
-//
-//  Created by  on 1/23/18.
-//  Copyright © 2018 deku. All rights reserved.
-//
+import SpriteKit
 
-import Foundation
+let BallCategoryName = "ball"
+let PaddleCategoryName = "paddle"
+let BlockCategoryName = "block"
+let GameMessageName = "gameMessage"
+
+
+class gameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        super.didMove(to: view)
+        //
+        let borderBody = SKPhysicsBody(edgeLoopFrom: self.frame)
+        borderBody.friction = 0
+        self.physicsBody = borderBody
+        //
+    }
+    
+    
+}
+
