ccaf1b59a1270bbcc6460cfee473848ff95ada4a
graffitigirl421 <caitcarp@yahoo.com>
2018-01-25 10:57:09 -0500

    sssss

--- DoYouKnowTheWay/GameScene.swift
+++ DoYouKnowTheWay/GameScene.swift
@@ -15,6 +15,7 @@ struct PhysicsCategory {
     static let topOrBottom: UInt32 = 0b10 // 000000000000000000000000000010
     static let paddle: UInt32 = 0b100 // 000000000000000000000000000100
     static let aiPaddle: UInt32 = 0b1000 // 00000000000000000000000001000
+    static let top: UInt32 = 0b10000
 }
 
 class GameScene: SKScene, SKPhysicsContactDelegate{
@@ -22,6 +23,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
     
     var myPaddle = SKSpriteNode()
     var ball = SKSpriteNode()
+    var block = SKSpriteNode()
     var aiPaddle = SKSpriteNode()
     var bottom = SKSpriteNode()
     var top = SKSpriteNode()
@@ -32,8 +34,15 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
     
     
     override func didMove(to view: SKView) {
+        // 1
+        let borderBody = SKPhysicsBody(edgeLoopFrom: self.frame)
+        // 2
+        borderBody.friction = 0
+        // 3
+        self.physicsBody = borderBody
         // Set the border of the world to the frame
-        let borderBody = SKPhysicsBody(edgeLoopFrom: frame)
+        _ = SKPhysicsBody(edgeLoopFrom: frame)
+        
         physicsBody?.friction = 0.0
         physicsBody = borderBody
         
@@ -43,15 +52,17 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
         myPaddle = childNode(withName: "paddle") as! SKSpriteNode
         ball = childNode(withName: "ball") as! SKSpriteNode
       
+        top = childNode(withName: "top") as! SKSpriteNode
         
         
         
         ball.physicsBody?.categoryBitMask = PhysicsCategory.ball
         myPaddle.physicsBody?.categoryBitMask = PhysicsCategory.paddle
+        top.physicsBody?.categoryBitMask = PhysicsCategory.top
         
         
         
-        ball.physicsBody?.contactTestBitMask = PhysicsCategory.topOrBottom
+        ball.physicsBody?.contactTestBitMask = PhysicsCategory.topOrBottom | PhysicsCategory.top
         physicsWorld.contactDelegate = self
     }
     
@@ -59,47 +70,51 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
     
     func didBegin(_ contact: SKPhysicsContact) {
         
-        if (contact.bodyA.categoryBitMask == PhysicsCategory.ball && contact.bodyB.categoryBitMask == PhysicsCategory.topOrBottom) || (contact.bodyB.categoryBitMask == PhysicsCategory.ball && contact.bodyA.categoryBitMask == PhysicsCategory.topOrBottom) {
-            print("ball hit top or bottom")
+      
+        
+        if (contact.bodyA.categoryBitMask == PhysicsCategory.ball && contact.bodyB.categoryBitMask == PhysicsCategory.top) || (contact.bodyB.categoryBitMask == PhysicsCategory.ball && contact.bodyA.categoryBitMask == PhysicsCategory.top) {
+            print("ball hit top ")
             
-            if contact.bodyB.node == top || contact.bodyA.node == top {
-                playerScore += 1
-                playerScoreLabel.text = String(playerScore)
-            } else {
-                computerScore += 1
-                computerScoreLabel.text = String(computerScore)
-            }
-            
-            if playerScore == 9 || computerScore == 0{
+            playerScore += 1
+            block.color = #colorLiteral(red: 0.4938244522, green: 0.6815513968, blue: 0.3626080453, alpha: 1)
+//            if contact.bodyB.node == top || contact.bodyA.node == top {
+//                playerScore += 1
+//                block.color = #colorLiteral(red: 0.4938244522, green: 0.6815513968, blue: 0.3626080453, alpha: 1)
+//
+//            }
+        
+            if playerScore == 1 {
+                // present gameOverScene
                 let gameOverScene = GameOverScene(size: self.size)
-                let reveal = SKTransition.crossFade(withDuration: 1)
+                let reveal = SKTransition.flipHorizontal(withDuration: 0.5)
                 view?.presentScene(gameOverScene, transition: reveal)
             }
-            
-            // stop the ball
-            ball.physicsBody!.velocity = CGVector.zero
-            
-            let wait = SKAction.wait(forDuration: 1.0)
-            let move = SKAction.run {
-                self.ball.position = CGPoint(x: self.frame.width * 0.5, y: self.frame.height * 0.5)
-            }
-            
-            let push = SKAction.run {
-                let array = [-300, -200, -150, -100, 100, 150, 200, 300]
-                
-                let randx = Int(arc4random_uniform(UInt32(array.count)))
-                let randy = Int(arc4random_uniform(UInt32(array.count)))
-                
-                self.ball.physicsBody!.applyImpulse(CGVector(dx: array[randx], dy: array[randy]))
-            }
-            let sequence = SKAction.sequence([wait, move, wait, push])
-            
-            run(sequence)
-            
+            restartBall()
         }
-     
     }
     
+    func restartBall() {
+        // stop the ball
+        ball.physicsBody!.velocity = CGVector.zero
+        // wait 1 second
+        let wait = SKAction.wait(forDuration: 1.0)
+        
+        // move to center
+        let moveBall = SKAction.run {
+            self.ball.position = CGPoint(x: self.frame.width * 0.5, y: self.frame.height * 0.5)
+        }
+        let pushBall = SKAction.run {
+            // push ball again
+            let array = [150, 200, 100, -150, -200, -100]
+            let randx = Int(arc4random_uniform(UInt32(array.count)))
+            let randy = Int(arc4random_uniform(UInt32(array.count)))
+            self.ball.physicsBody!.applyImpulse(CGVector(dx: array[randx], dy: array[randy]))
+        }
+        
+        let sequence = SKAction.sequence([wait, moveBall, wait, pushBall])
+        
+        run(sequence)
+    }
     
     
     
